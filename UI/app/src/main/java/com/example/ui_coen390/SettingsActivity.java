3b2668ba831df7d4c6c424911b564025c61b4765
johnnyboyy21 <jchiem007@hotmail.com>
2025-10-14 15:13:34 -0400

    Add files via upload

---
+++ UI/app/src/main/java/com/example/ui_coen390/SettingsActivity.java
@@ -0,0 +1,6 @@
+package com.example.ui_coen390;
+
+import android.app.Activity;
+
+public class SettingsActivity extends Activity {
+}
---
+++ UI/app/src/main/java/com/example/ui_coen390/StatsActivity.java
@@ -0,0 +1,6 @@
+package com.example.ui_coen390;
+
+import android.app.Activity;
+
+public class StatsActivity extends Activity {
+}
